851d568321cda451256a427b0c3f9f312f7d7fc1
Stephen Koch <stephen.koch@gmail.com>
2015-11-01 18:30:44 -0500

    debug

--- index.php
+++ index.php
@@ -7,17 +7,20 @@
     if( strpos( $_SERVER['HTTP_HOST'], 'herokuapp.com' ) !== false )
     {
       echo "<pre>"; print_r( 'heroku' ); echo "</pre>";
-      $m = new MongoClient( 'mongodb://skoch:n%Ub2yk.2?Ei>2B6FnLKP@ds045464.mongolab.com:45464/heroku_70gfb9l5' );
-      $db = $m->heroku_70gfb9l5;
-      $dongbot_collection = $db->dongles;
       echo "<pre>"; print_r( array( '$name' => $name) ); echo "</pre>";
+      $m = new MongoClient( 'mongodb://skoch:n%Ub2yk.2?Ei>2B6FnLKP@ds045464.mongolab.com:45464/heroku_70gfb9l5' );
+      echo "<pre>"; print_r( array( '$m' => $m) ); echo "</pre>";
+      $db = $m->heroku_70gfb9l5;
+      echo "<pre>"; print_r( array( '$db' => $db) ); echo "</pre>";
+      $dongbot_collection = $db->dongles;
+      echo "<pre>"; print_r( array( '$dongbot_collection' => $dongbot_collection) ); echo "</pre>";
     }else
     {
       $m = new MongoClient();
       $db = $m->dongles;
       $dongbot_collection = $db->dongbot;
     }
-    echo "<pre>"; print_r( '1' ); echo "</pre>";
+
     $dongle = $dongbot_collection->findOne(
       array( "Name" => $name )
     );
