9083964e46c9d5edd2e20b576519d58911f18918
Stephen Koch <stephen.koch@gmail.com>
2015-11-01 18:28:24 -0500

    testing

--- index.php
+++ index.php
@@ -10,13 +10,14 @@
       $m = new MongoClient( 'mongodb://skoch:n%Ub2yk.2?Ei>2B6FnLKP@ds045464.mongolab.com:45464/heroku_70gfb9l5' );
       $db = $m->heroku_70gfb9l5;
       $dongbot_collection = $db->dongles;
+      echo "<pre>"; print_r( array( '$name' => $name) ); echo "</pre>";
     }else
     {
       $m = new MongoClient();
       $db = $m->dongles;
       $dongbot_collection = $db->dongbot;
     }
-
+    echo "<pre>"; print_r( '1' ); echo "</pre>";
     $dongle = $dongbot_collection->findOne(
       array( "Name" => $name )
     );
