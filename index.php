ff63068958d403339d0d870cae8c75cd4c7263ec
Stephen Koch <stephen.koch@gmail.com>
2015-11-01 21:33:02 -0500

    debug

--- index.php
+++ index.php
@@ -4,22 +4,31 @@
   {
     $name = $_GET['n'];
 
-    if( strpos( $_SERVER['HTTP_HOST'], 'herokuapp.com' ) !== false )
-    {
-      echo "<pre>"; print_r( 'heroku' ); echo "</pre>";
-      echo "<pre>"; print_r( array( '$name' => $name) ); echo "</pre>";
-      $m = new MongoClient( 'mongodb://skoch:n%Ub2yk.2?Ei>2B6FnLKP@ds045464.mongolab.com:45464/heroku_70gfb9l5' );
-      echo "<pre>"; print_r( array( '$m' => $m) ); echo "</pre>";
-      $db = $m->heroku_70gfb9l5;
-      echo "<pre>"; print_r( array( '$db' => $db) ); echo "</pre>";
-      $dongbot_collection = $db->dongles;
-      echo "<pre>"; print_r( array( '$dongbot_collection' => $dongbot_collection) ); echo "</pre>";
-    }else
-    {
-      $m = new MongoClient();
-      $db = $m->dongles;
-      $dongbot_collection = $db->dongbot;
-    }
+    echo "<pre>"; print_r( 'heroku' ); echo "</pre>";
+    echo "<pre>"; print_r( array( '$name' => $name) ); echo "</pre>";
+    $m = new MongoClient( 'mongodb://skoch:n%Ub2yk.2?Ei>2B6FnLKP@ds045464.mongolab.com:45464/heroku_70gfb9l5' );
+    echo "<pre>"; print_r( array( '$m' => $m) ); echo "</pre>";
+    $db = $m->heroku_70gfb9l5;
+    echo "<pre>"; print_r( array( '$db' => $db) ); echo "</pre>";
+    $dongbot_collection = $db->dongles;
+    echo "<pre>"; print_r( array( '$dongbot_collection' => $dongbot_collection) ); echo "</pre>";
+
+    // if( strpos( $_SERVER['HTTP_HOST'], 'herokuapp.com' ) !== false )
+    // {
+    //   echo "<pre>"; print_r( 'heroku' ); echo "</pre>";
+    //   echo "<pre>"; print_r( array( '$name' => $name) ); echo "</pre>";
+    //   $m = new MongoClient( 'mongodb://skoch:n%Ub2yk.2?Ei>2B6FnLKP@ds045464.mongolab.com:45464/heroku_70gfb9l5' );
+    //   echo "<pre>"; print_r( array( '$m' => $m) ); echo "</pre>";
+    //   $db = $m->heroku_70gfb9l5;
+    //   echo "<pre>"; print_r( array( '$db' => $db) ); echo "</pre>";
+    //   $dongbot_collection = $db->dongles;
+    //   echo "<pre>"; print_r( array( '$dongbot_collection' => $dongbot_collection) ); echo "</pre>";
+    // }else
+    // {
+    //   $m = new MongoClient();
+    //   $db = $m->dongles;
+    //   $dongbot_collection = $db->dongbot;
+    // }
 
     $dongle = $dongbot_collection->findOne(
       array( "Name" => $name )
